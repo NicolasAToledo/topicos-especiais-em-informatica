7a5daeece82061f97353dba2bf2d4daadd463dd1
Nicolas Toledo <nicolas.toledo@fatec.sp.gov.br>
2020-05-07 20:57:57 -0300

    Criando o projeto e as classes Produto e ProdutoRepositorio

---
+++ src/main/java/com/topicos/demo/AulaTopicos1Application.java
@@ -0,0 +1,13 @@
+package com.topicos.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulaTopicos1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulaTopicos1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/topicos/demo/AulaTopicos1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.topicos.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulaTopicos1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

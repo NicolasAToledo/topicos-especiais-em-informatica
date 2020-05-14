59721b744f08d03362fe949a1f5718e132e1cc61
Nicolas Toledo <nicolas.toledo@fatec.sp.gov.br>
2020-05-13 21:26:57 -0300

    Recebendo dados do front-end

--- src/main/java/com/topicos/demo/ControladoraProduto.java
+++ src/main/java/com/topicos/demo/ControladoraProduto.java
@@ -2,13 +2,16 @@ package com.topicos.demo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class ControladoraProduto {
 
-	@GetMapping("/testando")
-	public String metodoTeste() {
-		System.out.println("Passei aqui!!!");
-		return "paginaTeste";	
+	@GetMapping("/testando")//Duas formas de receber dados | @RequestParam("id")Long id, 
+	public ModelAndView metodoTeste(Produto produto) {
+		System.out.println(produto.getId()+produto.getNome()+produto.getDescricao()+produto.getPreco());
+		ModelAndView modelAndView = new ModelAndView("paginaTeste");
+		modelAndView.addObject("produtoVariavel", produto);
+		return modelAndView;	
 	}
 }
